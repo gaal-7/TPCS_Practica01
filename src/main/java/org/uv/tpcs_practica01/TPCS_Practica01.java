894806d64926a2b81eb1a5b3814b51224fed9e23
sirma <sirma@gaal>
2023-09-14 11:21:22 -0600

    Mi primer commit

---
+++ src/main/java/org/uv/tpcs_practica01/TPCS_Practica01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.uv.tpcs_practica01;
+
+/**
+ *
+ * @author sirma
+ */
+public class TPCS_Practica01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
