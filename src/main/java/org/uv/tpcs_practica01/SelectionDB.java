286fac9d09db1f5a1fc911c1fb11c8aee42694f1
sirma <sirma@gaal>
2023-09-18 11:31:32 -0600

    agregando clases

---
+++ src/main/java/org/uv/tpcs_practica01/SelectionDB.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.tpcs_practica01;
+
+import java.util.List;
+import java.sql.Connection;
+
+public abstract class SelectionDB<T> {
+    protected T p;
+    
+    public SelectionDB(T p) {
+        this.p = p;
+    }
+    public abstract List<T> select(Connection con);    
+}
