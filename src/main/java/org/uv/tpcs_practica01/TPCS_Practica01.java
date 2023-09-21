c32ef944c8e1511e94fa9d642606c5c31d860176
Omarotierra <2omarhernandez2@gmail.com>
2023-09-21 08:18:55 -0500

    TPCS_Practica01.java

---
+++ src/main/java/org/uv/tpcs_practica01/TPCS_Practica01.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.tpcs_practica01;
+
+/**
+ *
+ * @author 2omar
+ */
+public class TPCS_Practica01 {
+    
+}
